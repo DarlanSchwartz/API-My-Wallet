48e5b3e17f74eefc153124729adac5cee2d3d511
Schwartz <darlanswtz@gmail.com>
2023-07-06 23:16:53 -0300

    Feat: Edit transactions working

--- src/app.js
+++ src/app.js
@@ -218,10 +218,63 @@ app.delete('/deletar-registro/:id', async (req, res) => {
 });
 
 app.put('/editar-registro/:id', async (req, res) => {
-    const token = req.headers.token;
 
-
+    const token = req.headers.authorization;
     if (!token) return res.sendStatus(401);
 
+    const id = req.params.id;
+    if(!id || id == '') return res.status(404).send('É necessário um id para editar uma transação!');
+
+    const {value,description} = req.body;
+
+    const editTransactionSchema = joi.object({
+        value: joi.number().positive().required(),
+        description: joi.string().required(),
+    });
+
+    const hasError = editTransactionSchema.validate(req.body, { abortEarly: false }).error;
+
+    if (hasError) {
+        let errorMessage = '';
+
+        if (isNaN(Number(value))) errorMessage = 'Valor não pode ser uma string em uma transação';
+
+        if (!isNaN(Number(value) && Number(value) < 0)) errorMessage += '\nValor não pode ser negativo em uma transação';
+
+        if (description == undefined || description.length == 0) errorMessage += '\nDescrição da transação não pode estar vazia';
+
+        return res.status(422).send({ error: hasError.details, message: errorMessage});
+    }
+    
+    try {
+        const foundUser = await db.collection('sessions').findOne({ token: token.replace('Bearer ', '') });
+        if (!foundUser) return res.status(401).send({ message: 'Usuário não está logado!' });
+
+        const userInfo = await db.collection('users').findOne({ email: foundUser.email });
+        if (!userInfo) return res.status(401).send({ message: 'Usuário não existe!' });        
+
+        let updatedTransaction = userInfo.transactions.find(tr => tr.id == id);
+        updatedTransaction.value = value;
+        updatedTransaction.description = description;
+
+        let newBalance = 0;
+
+        userInfo.transactions.forEach(trans => {
+            if (trans.type == 'entrada') {
+                newBalance += Number(trans.value);
+            }
+            else {
+                newBalance -= Number(trans.value);
+            }
+        });
+
+        await db.collection('users').updateOne({ email: foundUser.email }, { $set: { transactions: userInfo.transactions, balance: newBalance } });
+
+        return res.status(202).send({ transactions: userInfo.transactions, balance: newBalance});
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({ message: 'Internal server error' });
+    }
 });
 
